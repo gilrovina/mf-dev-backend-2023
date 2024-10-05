74808144e75b09c0d9625ba9b5a171dfc591f718
gilrovina <164437575+gilrovina@users.noreply.github.com>
2024-10-05 15:11:52 -0300

    Configuração do Banco de Dados e da Tabela Veículo

--- mf-dev-backend-2023/Program.cs
+++ mf-dev-backend-2023/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using mf_dev_backend_2023.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
